d17b636ecc3a27502d9d5627d250a8b1ff96614c
Michael James <mikeyj777@gmail.com>
2024-08-03 13:13:14 -0400

    training on cartpole

--- agent.py
+++ agent.py
@@ -24,6 +24,12 @@ class Agent:
         self.epsilon_init = hyperparameters['epsilon_init']
         self.epsilon_decay = hyperparameters['epsilon_decay']
         self.epsilon_min = hyperparameters['epsilon_min']
+        self.network_sync_rate = hyperparameters['network_sync_rate']
+        self.learning_rate_a = hyperparameters['learning_rate_a']
+        self.discount_factor_g = hyperparameters['discount_factor_g']
+
+        self.loss = torch.nn.MSELoss()
+        self.optimizer = None
 
     def run(self, is_training=True, render=False):
         
@@ -36,11 +42,18 @@ class Agent:
         num_states = env.observation_space.shape[0]
 
         policy_dqn = DQN(num_states, num_actions).to(device)
-
+        
         epsilon = self.epsilon_init
 
         if is_training:
             memory = ReplayMemory(self.replay_memory_size)
+            target_dqn = DQN(num_states, num_actions).to(device)
+            target_dqn.load_state_dict(policy_dqn.state_dict())
+
+            training_steps = 0
+
+            self.optimizer = torch.optim.Adam(policy_dqn.parameters(), lr=self.learning_rate_a)
+
         
         rewards_per_episode = []
         epsilon_history = []
@@ -64,12 +77,18 @@ class Agent:
                 # Processing:
                 new_state, reward, terminated, _, info = env.step(action.item())
 
+                if terminated:
+                    reward = -300
+
                 new_state = torch.tensor(new_state, device=device, dtype=torch.float32)
                 reward = torch.tensor(reward, device=device, dtype=torch.float32)
                 
                 episode_reward += reward
                 if is_training:
                     memory.append((state, action, new_state, reward, terminated))
+
+                    training_steps += 1
+                    # print(training_steps)
 
                 state = new_state
             
@@ -79,8 +98,35 @@ class Agent:
             epsilon = max(self.epsilon_min, epsilon * self.epsilon_decay)
             epsilon_history.append(epsilon)
 
+            if is_training and len(memory) >= self.mini_batch_size:
+                mini_batch = memory.sample(self.mini_batch_size)
+                self.optimize(mini_batch, policy_dqn, target_dqn)
 
+                if training_steps > self.network_sync_rate:
+                    target_dqn.load_state_dict(policy_dqn.state_dict())
+                    training_steps = 0
+                
         env.close()
+
+    def optimize(self, mini_batch, policy_dqn, target_dqn):
+
+        states, actions, new_states, rewards, terminateds = zip(*mini_batch)
+        states = torch.stack(states)
+        actions = torch.stack(actions)
+        new_states = torch.stack(new_states)
+        rewards = torch.stack(rewards)
+        terminations = torch.tensor(terminateds).float().to(device)
+
+        with torch.no_grad():
+            target_q = rewards + (1-terminations) * self.discount_factor_g * target_dqn(new_states).max(dim=1)[0]
+
+        
+        current_q = policy_dqn(states).gather(dim = 1, index=actions.unsqueeze(dim=1)).squeeze()
+        loss = self.loss(current_q, target_q)
+        
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
 if __name__ == "__main__":
     agent = Agent('cartpole1')
