8443e6a31b9c319e4c303f01bb02cceffc856810
Michael James <mikeyj777@gmail.com>
2024-08-04 12:16:29 -0400

    helper

--- helpers.py
+++ helpers.py
@@ -1,11 +1,11 @@
 import tkinter as tk
 from tkinter import filedialog
 
-def get_path_to_trained_model():
+def get_path_to_trained_model(initialdir='runs'):
 
     root = tk.Tk()
     root.withdraw()
 
-    file_path = filedialog.askopenfilename(initialdir='runs', title='Select trained model')
+    file_path = filedialog.askopenfilename(initialdir=initialdir, title='Select trained model')
 
     return file_path
